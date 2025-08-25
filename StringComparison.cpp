aa0b03ccddab1c5bf061093a74012c0ec5d340f7
Raniya <miahraniya@gmail.com>
2025-08-24 20:47:50 -0500

    Final

--- StringComparison.cpp
+++ StringComparison.cpp
@@ -2,26 +2,29 @@
 #include <string>
 using namespace std;
 
-void strcmp_case_insensitive(string str1, string str2);
+int strcmp_case_insensitive(string str1, string str2);
 
 int main() {
     
-    strcmp_case_insensitive("", "");
+    strcmp_case_insensitive("string one", "string two");
+    strcmp_case_insensitive("string one", "string one");
+    strcmp_case_insensitive("string two", "string one");
 
     return 0;
 }
 
-void strcmp_case_insensitive(string str1, string str2){
-    
+int strcmp_case_insensitive(string str1, string str2){
+ 
     if (str1 < str2){
-        cout << "The comparison of 'String one' and 'string two' returns -1.\n";
+        cout << "The comparison of '" << str1 << "' and '" << str2 << "' returns -1.\n";
         return -1;
     }else if(str1 == str2){
-        cout << "The comparison of 'String one' and 'string one' returns 0.\n";
+        cout << "The comparison of '" << str1 << "' and '" << str2 << "' returns 0.\n";
         return 0;
     }else if(str1 > str2){
+        cout << "The comparison of '" << str1 << "' and '" << str2 << "' returns 1.\n";
         return 1;
     }
-    
 
+    return 0;
 }
