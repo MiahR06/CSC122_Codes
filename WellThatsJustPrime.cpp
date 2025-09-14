5fc69bd561fd7a4c33aac3cce7bd8a8d3ef4ade9
Raniya <miahraniya@gmail.com>
2025-09-14 15:54:07 -0500

    Update WellThatsJustPrime.cpp

--- WellThatsJustPrime.cpp
+++ WellThatsJustPrime.cpp
@@ -2,33 +2,45 @@
 #include <vector>
 using namespace std;
 
-int primeFactorsHelper(int n, int divisor, vector<int> arrayList);
-int printArray(vector<int> arrayList);
-//int primeFactor(int n);
+int primeFactors(int n);
+int primeFactorsHelper(int n, int d, vector<int> arrayList);    // Helper function
+int printArray(vector<int> arrayList);                          // Function to print vector
 
 int main(){
 
-    //primeFactors(10);
+    primeFactors(0);
+    primeFactors(1);
+    primeFactors(9);
+    primeFactors(10);
+
+    primeFactors(100);
     
     return 0;
 }
 
-int primeFactorsHelper(int n, int divisor, vector<int> arrayList){
+int primeFactors(int n){
+    vector<int> arrayList;          // vector to hold factors
+    return primeFactorsHelper(n, 2, arrayList);
+}
+
+int primeFactorsHelper(int n, int d, vector<int> arrayList){
 
     if (n <= 1){
-        return printArray(arrayList);
-    }else if(n % divisor == 0){
-        arrayList.push_back(divisor);
-        return primeFactorsHelper(n/divisor, divisor, arrayList);
+        return printArray(arrayList);       // Calls function to print vector if the number has no factors
+    }else if((n % d) == 0){                 
+        arrayList.push_back(d);             // Adds divisor to vector
+        return primeFactorsHelper(n/d, d, arrayList);        // Calls function again with a smaller n
+    }else{
+        return primeFactorsHelper(n, d+1, arrayList);        // Moves onto next divisor
     }
+
+    return 1;
 }
 
 int printArray(vector<int> arrayList){
     for (int i=0; i<arrayList.size(); i++){
         cout << arrayList[i] << "  ";
     }
+    cout << endl << endl;
+    return 1;
 }
-
-/*int primeFactor(int n){
-
-}*/
