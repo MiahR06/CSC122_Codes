1c49c3693dfa7985a6fa01fb672644f3183b0ba3
Raniya <miahraniya@gmail.com>
2025-10-12 23:31:14 -0500

    Update Week8_TemplatingLab.cpp

--- Week8_TemplatingLab.cpp
+++ Week8_TemplatingLab.cpp
@@ -4,60 +4,55 @@
 #include <cassert>
 using namespace std;
 
-template<typename T> class Functions{
-public:
+template<class T> vec_fill(T elem){
     vector<T> vec;
     int addToElem = 0;
+    for (int i=0; i <= 10; i++){
+        vec.push_back(elem + addToElem);
+        addToElem++;
+    }
+    return elem;
+}
 
-    void vec_fill(T elem){
-        for (int i=0; i <= 10; i++){
-            vec.push_back(elem + addToElem);
-            addToElem++;
+template<class T>
+until(T elem, function<T(T)> func, function<bool(T)> predicate){
+    while(!predicate(elem)){
+        elem = func(elem);
+    }
+}
+
+template<class T> order(T& a, T& b, T& c){
+    T hold;
+    if (a > b && a > c){
+        a = a;
+        if (b > c){
+            b = b;
+        }else if (c > b){
+            hold = c;
+            c = b;
+        }
+    }else if (b > a && b > c){
+        hold = a;
+        a = b;
+        if (a > c){
+            b = hold;
+        }else if (c > a){
+            b = c;
+            c = hold;
+        }
+    }else if (c > a && c > b){
+        hold = a;
+        a = c;
+        if (a > b){
+            c = b;
+            b = hold;
+        }else if (b > a){
+            a = hold;
         }
     }
-
-    void until(T elem, function<T(T)> func, function<bool(T)> predicate){
-        while(!predicate(elem)){
-            elem = func(elem);
-        }
-    }
-
-    void order(T& a, T& b, T& c){
-        T hold;
-        if (a > b && a > c){
-            a = a;
-            if (b > c){
-                b = b;
-            }else if (c > b){
-                hold = c;
-                c = b;
-            }
-        }else if (b > a && b > c){
-            hold = a;
-            a = b;
-            if (a > c){
-                b = hold;
-            }else if (c > a){
-                b = c;
-                c = hold;
-            }
-        }else if (c > a && c > b){
-            hold = a;
-            a = c;
-            if (a > b){
-                c = b;
-                b = hold;
-            }else if (b > a){
-                a = hold;
-            }
-        }
-
-    }
-};
+}
 
 int main(){
-
-    
 
     return 0;
 }
